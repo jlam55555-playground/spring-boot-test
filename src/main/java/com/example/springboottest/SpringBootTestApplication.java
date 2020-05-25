0c64ad2e4a9649002b22c785e90d244c3b6718ac
Jonathan Lam <jlam55555@gmail.com>
2020-05-25 04:51:33 +0000

    added basic controllers

--- src/main/java/com/example/springboottest/SpringBootTestApplication.java
+++ src/main/java/com/example/springboottest/SpringBootTestApplication.java
@@ -2,11 +2,19 @@ package com.example.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class SpringBootTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTestApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootTestApplication.class, args);
+
+        // for 404 error handling
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 }
